a674a468a33ca95d8ed44c700667e0b1ffd1aa34
unknown <khabibulaev_k@12-03.iuca.loc>
2016-09-15 17:10:27 +0600

    dog

---
+++ quiz2.py
@@ -0,0 +1,126 @@
+print "welcome in our game"
+random import
+chislo1 = int(input("vyberite chislo ot 0 do 6"))
+comp = random.randint(1,6)
+if chislo > 6:
+	    print "vyberite chislo ot 1 do 6"
+elif comp  < 0:
+		print "vyberite chislo ot 1 do 6"
+elif comp == chiclo1:
+		x  = kh - chislo1
+		ball = 6
+		print "Tvoi kolichetvo ballov:"
+		print ball
+
+elif comp > chislo1:
+		x = comp - chislo1
+		if x == 1 :
+				ball = ball + 5
+				print "Tvoi kolichetvo ballov:"
+				print ball 
+		elif x == 2 :
+				ball = ball + 4
+				print "Tvoi kolichetvo ballov:"
+				print ball
+		elif x == 3 :
+			 	ball = ball + 3 
+			 	print "Tvoi kolichetvo ballov:"
+			 	print ball 
+		elif x == 4 :
+				ball = ball + 2
+				print "Tvoi kolichetvo ballov:"
+		elif x == 5 :
+		  		ball = ball + 1
+		  		print "Tvoi kolichetvo ballov:"
+		  		print ball 
+
+elif comp < chislo2 :
+		x = comp - chislo1
+		if x == 1 :
+				ball = ball + 5
+				print "Tvoi kolichetvo ballov:"
+				print ball 
+		elif x == 2 :
+				ball = ball + 4
+				print "Tvoi kolichetvo ballov:"
+				print ball
+		elif x == 3 :
+			 	ball = ball + 3 
+			 	print "Tvoi kolichetvo ballov:"
+			 	print ball 
+		elif x == 4 :
+				ball = ball + 2
+				print "Tvoi kolichetvo ballov:"
+		elif x == 5 :
+		  		ball = ball + 1
+		  		print "Tvoi kolichetvo ballov:"
+		  		print ball 
+  
+ elif comp > chislo3:
+        x = comp - chislo1
+        if x == 1 :
+	 			ball = ball + 5
+				print "Tvoi kolichetvo ballov:"
+				print ball 
+		elif x == 2 :
+				ball = ball + 4
+				print "Tvoi kolichetvo ballov:"
+				print ball
+		elif x == 3 :
+			 	ball = ball + 3 
+			 	print "Tvoi kolichetvo ballov:"
+			 	print ball 
+		elif x == 4 :
+				ball = ball + 2
+				print "Tvoi kolichetvo ballov:"
+		elif x == 5 :
+		  		ball = ball + 1
+		  		print "Tvoi kolichetvo ballov:"
+		  		print ball 
+
+ elif comp > chislo4:
+        x = comp - chislo1
+        if x == 1 :
+	 			ball = ball + 5
+				print "Tvoi kolichetvo ballov:"
+				print ball 
+		elif x == 2 :
+				ball = ball + 4
+				print "Tvoi kolichetvo ballov:"
+				print ball
+		elif x == 3 :
+			 	ball = ball + 3 
+			 	print "Tvoi kolichetvo ballov:"
+			 	print ball 
+		elif x == 4 :
+				ball = ball + 2
+				print "Tvoi kolichetvo ballov:"
+		elif x == 5 :
+		  		ball = ball + 1
+		  		print "Tvoi kolichetvo ballov:"
+		  		print ball 
+		 
+
+   elif comp > chislo5:
+        x = comp - chislo1
+        if x == 1 :
+	 			ball = ball + 5
+				print "Tvoi kolichetvo ballov:"
+				print ball 
+		     elif x == 2 :
+		 		ball = ball + 4
+				print "Tvoi kolichetvo ballov:"
+				print ball
+		     elif x == 3 :
+			 	ball = ball + 3 
+			 	print "Tvoi kolichetvo ballov:"
+			 	print ball 
+		     elif x == 4 :
+				ball = ball + 2
+				print "Tvoi kolichetvo ballov:"
+		     elif x == 5 :
+		  		ball = ball + 1
+		  		print "Tvoi kolichetvo ballov:"
+		  		print ball 
+		 
+
